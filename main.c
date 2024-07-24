64d3a044382a786a53be4c9fb80f1805ac56af04
NarcoMarco <marcocondipodero@gmail.com>
2024-07-24 13:39:53 +1000

    Basic - no file cache yet

--- main.c
+++ main.c
@@ -4,9 +4,31 @@
 typedef struct books {
     int ID;
     char* Title;
-    int Author;
+    char* Author;
 } Books;
 
+typedef struct list {
+    Books* book;
+    Books* next;
+} List;
+
+List add(List* head) {
+    List* newBook = head;
+    int ID = 0;
+    for(int i = 0; newBook->next != NULL; i++) {
+        if(newBook->next != NULL) {
+            newBook = newBook->next;
+            ID++;
+        }
+    }
+    newBook->book->ID = ID;
+    char Title[100];
+    printf("Enter the book title: ");
+    scanf("%s", Title);
+ }
+
 int main() {
+    List* head = NULL;
+    *head = add(head);
     return 0;
 }
