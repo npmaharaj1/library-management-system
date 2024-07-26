e3f95112d4b3b8227cc130ecaf503e824a15d7d2
NarcoMarco <marcocondipodero@gmail.com>
2024-07-26 10:56:12 +1000

    Added header file to hold structs and function definitions

--- main.c
+++ main.c
@@ -2,37 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h> // this will be needed for date handling later
-
-#define MAXTITLELENGTH 100 // Max length a books title can be
-#define MAXAUTHORLENGTH 50 // Max length an Authors name can be
-#define MAXNAMELENGTH 50 // Max length a name can be
-#define MAXEMAILLENGTH 100 // Max length an email can be
-
-typedef struct date { // this might need to go in a separate time.c file so that we can separate out the date handling later.
-    int day;
-    int month;
-    int year;
-} Date;
-
-typedef struct person {
-    int ID;
-    char name[MAXNAMELENGTH];
-    char email[MAXEMAILLENGTH];
-} Person;
-
-typedef struct books {
-    int ID;
-    char Title[MAXTITLELENGTH];
-    char Author[MAXAUTHORLENGTH];
-    Date* issueDate;
-    Date* dueDate; 
-    Person* borrowedTo; // name of person who has borrowed the book.   
-} Book;
-
-typedef struct list {
-    Book* book;
-    struct list* next;
-} List;
+#include "otherFunctions.h"
 
 //Read From File Function
 List* readBooksFromFile(const char* booklist) {
