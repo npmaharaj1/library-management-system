4c4e5deadc15661c90ffbab8d8945da37bebc499
Nishant <npmaharaj1@gmail.com>
2024-07-31 18:41:37 +1000

    Inverted Colour support

--- menu.c
+++ menu.c
@@ -35,13 +35,24 @@ void getArrowKey(int ch) {
     }
 }
 
-void displayOptions(char *options[3], const char *prompt) {
+void displayOptions(char *options[3], const char *prompt, int selectedItemIndex) {
     printw("%s", prompt);
     for (int i = 0; i < 3; i++) {
         char *currentOption = options[i];
-        printw("\n<< %s >>", currentOption);
+        char prefix;
+        if (selectedItemIndex == i) {
+            prefix = '*';
+            attron(COLOR_PAIR((2)));
+        } else {
+            prefix = ' ';
+            attron(COLOR_PAIR((1)));
+        }
+        printw("\n%c << %s >>", prefix, currentOption);
     }
     printw("\n");
+    attroff(COLOR_PAIR(1));
+    attroff(COLOR_PAIR(2));
+    
 }
 
 int main() {
@@ -52,12 +63,22 @@ int main() {
     keypad(stdscr, TRUE); // Enable keypad
     int ch; // keypad data
 
+    // Initialize colours
+    if (has_colors() == FALSE) {
+        endwin();
+        printf("Your terminal does not support colours\n"); // IMPLIMENT SKIP LATER
+        return 1;
+    }
+    start_color();
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+    init_pair(2, COLOR_BLACK, COLOR_WHITE);
+
     // Visual Stuff
-    int selectedItemIndex = 0; // Selected menu item in index form
+    int selectedItemIndex = 2; // Selected menu item in index form
     const char *prompt = "Welcome, what would you like to do"; // Instructions for user
     char *options[3] = {"Play", "About", "Exit"};
 
-    displayOptions(options, prompt);
+    displayOptions(options, prompt, selectedItemIndex);
     printw("Press any key to exit...\n");
     refresh();
 
